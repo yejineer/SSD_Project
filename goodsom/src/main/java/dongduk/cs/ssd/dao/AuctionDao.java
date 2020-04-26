3934f0b225fa36c8c9098b06d8cbbbe342de8072
hyekyung-kim <khatcong@gmail.com>
2020-04-26 14:57:32 +0900

    ENH: Auction and Bid DAO by HK

--- goodsom/src/main/java/dongduk/cs/ssd/dao/AuctionDao.java
+++ goodsom/src/main/java/dongduk/cs/ssd/dao/AuctionDao.java
@@ -1,5 +1,21 @@
 package dongduk.cs.ssd.dao;
 
-public interface AuctionDao {
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+
+import dongduk.cs.ssd.domain.Auction;
+import dongduk.cs.ssd.domain.Bid;
+
+public interface AuctionDao {
+	Auction getAuction(int auctionId) throws DataAccessException;
+	Auction getAuction(int auctionId, int menuId) throws DataAccessException;
+	
+	void createAuction(Auction auction) throws DataAccessException;
+	void updateAuciton(Auction auction) throws DataAccessException;
+	void deleteAuction(Auction auction) throws DataAccessException;
+	
+	List<Bid> getBidByAuctionId(int auctionId) throws DataAccessException;
+	boolean isAuctionClosed(int auctionId, Date endDate) throws DataAccessException;
 }
--- goodsom/src/main/java/dongduk/cs/ssd/dao/BidDao.java
+++ goodsom/src/main/java/dongduk/cs/ssd/dao/BidDao.java
@@ -1,5 +1,17 @@
 package dongduk.cs.ssd.dao;
 
-public interface BidDao {
+import org.springframework.dao.DataAccessException;
 
+import dongduk.cs.ssd.domain.Bid;
+import dongduk.cs.ssd.domain.User;
+
+
+public interface BidDao {
+	Bid getBid(int bidId) throws DataAccessException;
+	Bid getBid(int bidId, int auctionId) throws DataAccessException;
+	
+	void createBid(Bid bid) throws DataAccessException;
+	void updateBid(Bid bid) throws DataAccessException;
+	
+	User getUserById(int userId) throws DataAccessException;
 }
