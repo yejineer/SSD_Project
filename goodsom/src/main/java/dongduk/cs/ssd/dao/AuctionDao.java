58158a74e7133252afd6a1e8862c2d3daae1d512
hyekyung-kim <khatcong@gmail.com>
2020-04-26 16:13:39 +0900

    ENH: Auction edited by HK

--- goodsom/src/main/java/dongduk/cs/ssd/dao/AuctionDao.java
+++ goodsom/src/main/java/dongduk/cs/ssd/dao/AuctionDao.java
@@ -7,15 +7,23 @@ import org.springframework.dao.DataAccessException;
 
 import dongduk.cs.ssd.domain.Auction;
 import dongduk.cs.ssd.domain.Bid;
+import dongduk.cs.ssd.domain.GroupBuy;
 
 public interface AuctionDao {
 	Auction getAuction(int auctionId) throws DataAccessException;
-	Auction getAuction(int auctionId, int menuId) throws DataAccessException;
 	
 	void createAuction(Auction auction) throws DataAccessException;
+	
 	void updateAuciton(Auction auction) throws DataAccessException;
+	
 	void deleteAuction(Auction auction) throws DataAccessException;
 	
+	
+	List<Auction> getAuctionList() throws DataAccessException;
+	
+	List<Auction> getAuctionListByKeyword(String keyword) throws DataAccessException;
+	
 	List<Bid> getBidByAuctionId(int auctionId) throws DataAccessException;
+	
 	boolean isAuctionClosed(int auctionId, Date endDate) throws DataAccessException;
 }
