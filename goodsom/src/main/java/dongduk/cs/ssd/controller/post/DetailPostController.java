29a57b42e479b8ec6f357c4dcb47848584e9fa51
kimdahyee <diana6777@naver.com>
2020-05-02 13:22:33 +0900

    ENH: controller/service.post, controller/service.comment, controller/service.scrap By DH

---
+++ goodsom/src/main/java/dongduk/cs/ssd/controller/post/DetailPostController.java
@@ -0,0 +1,5 @@
+package dongduk.cs.ssd.controller.post;
+
+public class DetailPostController {
+
+}
---
+++ goodsom/src/main/java/dongduk/cs/ssd/service/comment/CommentService.java
@@ -0,0 +1,5 @@
+package dongduk.cs.ssd.service.comment;
+
+public interface CommentService {
+
+}
