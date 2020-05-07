ecbc3738a6ef70ee0cbc9d129d818342e052e7b7
kimdahyee <diana6777@naver.com>
2020-05-08 06:30:06 +0900

    ENH:Update ListMyPageController By DH

--- goodsom/src/main/java/dongduk/cs/ssd/controller/mypage/ListMypageController.java
+++ goodsom/src/main/java/dongduk/cs/ssd/controller/mypage/ListMypageController.java
@@ -1,5 +1,34 @@
 package dongduk.cs.ssd.controller.mypage;
 
-public class ListMypageController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
+import dongduk.cs.ssd.controller.user.UserSession;
+import dongduk.cs.ssd.service.ScrapService;
+
+/**
+ * @author kimdahyee
+ * @since 05.08.2020
+ */
+
+@Controller
+@SessionAttributes("userSession")
+public class ListMypageController {
+	
+	@Autowired
+	ScrapService scrapService;
+	
+	@RequestMapping("/mypage/list.do")
+	public ModelAndView handleRequest(
+			@ModelAttribute("userSession") UserSession userSession) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("user/mypage_list");
+		mav.addObject("scrap_aList", scrapService.getScrap_aList());
+		mav.addObject("scrap_gList", scrapService.getScrap_gList());
+		return mav;
+	}
 }
