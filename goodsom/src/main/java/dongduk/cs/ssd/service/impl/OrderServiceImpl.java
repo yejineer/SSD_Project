2dbddf82553132be85e879b8a400c3428aa3d0d3
Seonmi-Hwang <hsm3530@gmail.com>
2020-06-13 11:19:38 +0900

    ENH: Add remark and method by SM

--- goodsom/src/main/java/dongduk/cs/ssd/service/impl/OrderServiceImpl.java
+++ goodsom/src/main/java/dongduk/cs/ssd/service/impl/OrderServiceImpl.java
@@ -11,6 +11,7 @@ import dongduk.cs.ssd.dao.OrderDao;
 import dongduk.cs.ssd.dao.UserDao;
 import dongduk.cs.ssd.domain.Auction;
 import dongduk.cs.ssd.domain.GroupBuy;
+import dongduk.cs.ssd.domain.LineGroupBuy;
 import dongduk.cs.ssd.domain.Order;
 import dongduk.cs.ssd.domain.User;
 import dongduk.cs.ssd.service.OrderService;
@@ -21,41 +22,43 @@ import dongduk.cs.ssd.service.OrderService;
  */
 
 @Service
-public class OrderServiceImpl { // implements OrderService {
+public class OrderServiceImpl {//implements OrderService {
 
-	
-	@Autowired
-	private OrderDao orderDao;
+//	@Autowired
+//	private OrderDao orderDao;
 //	@Autowired
 //	private GroupBuyDao groupBuyDao;
 //	@Autowired
 //	private AuctionDao auctionDao;
 //	@Autowired
 //	private UserDao userDao;
-//	
-//	
+//
 //	public User getUser(String emailId) {
 //		return userDao.getUserByEmailId(emailId);
 //	}
-//	
+//
 //	public GroupBuy getGroupBuy(int groupBuyId) {
 //		return groupBuyDao.getGroupBuy(groupBuyId);
 //	}
-//	
+//
 //	public Auction getAuction(int auctionId) {
 //		return auctionDao.getAuction(auctionId);
 //	}
-//	
+//
 //	public Order getOrder(int orderId) {
 //		return orderDao.getOrder(orderId);
 //	}
-//	
+//
 //	public void createOrder(Order order) {
 //		orderDao.createOrder(order);
 //	}
-//	
+//
 //	public List<Order> getOrderList(int userId) {
 //		return orderDao.getOrderList(userId);
 //	}
-	
+//	
+//	public LineGroupBuy getLineGroupBuy(int orderId) {
+//		return orderDao.getLineGroupBuy(orderId);
+//	}
+
 }
