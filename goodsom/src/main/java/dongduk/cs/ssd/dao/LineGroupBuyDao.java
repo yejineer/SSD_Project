fa95a04e037dd6648f84de2a67368c8ac37b8746
Seonmi-Hwang <hsm3530@gmail.com>
2020-06-24 18:30:50 +0900

    ENH: Create and implement LineGroupBuy dao by SM

---
+++ goodsom/src/main/java/dongduk/cs/ssd/dao/LineGroupBuyDao.java
@@ -0,0 +1,8 @@
+package dongduk.cs.ssd.dao;
+
+import dongduk.cs.ssd.domain.LineGroupBuy;
+
+public interface LineGroupBuyDao {
+	
+	void insertLineGroupBuy(LineGroupBuy lineGroupBuy);
+}
---
+++ goodsom/src/main/java/dongduk/cs/ssd/dao/mybatis/mapper/LineGroupBuyMapper.java
@@ -0,0 +1,8 @@
+package dongduk.cs.ssd.dao.mybatis.mapper;
+
+import dongduk.cs.ssd.domain.LineGroupBuy;
+
+public interface LineGroupBuyMapper {
+	
+	void insertLineGroupBuy(LineGroupBuy lineGroupBuy);
+}
