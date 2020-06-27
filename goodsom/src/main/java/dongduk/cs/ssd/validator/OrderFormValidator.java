78a4ce504df8d719ad28cc27478c0628352b5abf
Seonmi-Hwang <hsm3530@gmail.com>
2020-06-28 00:42:39 +0900

    ENH: Implement order form validation by SM

--- goodsom/src/main/java/dongduk/cs/ssd/validator/OrderFormValidator.java
+++ goodsom/src/main/java/dongduk/cs/ssd/validator/OrderFormValidator.java
@@ -6,6 +6,11 @@ import org.springframework.validation.Validator;
 
 import dongduk.cs.ssd.controller.order.OrderForm;
 
+/**
+ * @author Seonmi Hwang
+ * @since 2020.06.27
+ */
+
 public class OrderFormValidator implements Validator {
 
 	@Override
@@ -16,7 +21,6 @@ public class OrderFormValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		OrderForm regReq = (OrderForm) target;
-
 
 		// 필수 입력 항목
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "order.cardBank", "required");
@@ -29,27 +33,27 @@ public class OrderFormValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "order.phone", "required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "order.refundBank", "required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "order.refundAccount", "required");
-
-		
-//		String zipcode = regReq.getAddress().getZipcode();
-//		if (!zipcode.matches("^\\d{5}$")) { // zipcode가 5자리 숫자인지 검증
-//			errors.pushNestedPath("address");
-//			try {
-//				errors.rejectValue("zipcode", "typeMismatch");
-//			} finally {
-//				errors.popNestedPath();
-//			}
-//		}
 		
 		String cardNo = regReq.getOrder().getCardNo();
 		if (!cardNo.equals("") && !cardNo.matches("^\\d{4}-\\d{4}-\\d{4}-\\d{4}$")) {
 			errors.rejectValue("order.cardNo", "typeMismatch"); // xxxx-xxxx-xxxx-xxxx인지 검증
 		}
 		
+		String validDate = regReq.getOrder().getValidDate();
+		if (!validDate.equals("") && !validDate.matches("^(0[1-9]|1[0-2])/[0-9]{2}$")) {
+			errors.rejectValue("order.validDate", "typeMismatch"); // MM/YY 인지 검증
+		}
+		
+		String cvc = regReq.getOrder().getCvc();
+		if (!cvc.equals("") && !cvc.matches("^\\d{3}$")) {
+			errors.rejectValue("order.cvc", "typeMismatch"); // 세 자리 숫자인지 검증
+		}
+		
 		String phone = regReq.getOrder().getPhone();
 		if (!phone.equals("") && !phone.matches("^[0][1]\\d{1}-\\d{3,4}-\\d{4}$")) {
 			errors.rejectValue("order.phone", "typeMismatch"); // 01x-xxx-xxxx or 01x-xxxx-xxxx인지 검증
 		}
+
 	}
 
 }
